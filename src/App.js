5b7e22e812c3d38d94b2784b463d2d2e9e435621
Ahmed Saeed <ahmed.saeed@tenpearls.com>
2020-01-20 19:04:15 +0500

    git init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  login: new firebase.auth.EmailAuthProvider(),
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default withFirebaseAuth({ providers, firebaseAppAuth, })(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4mYvAgHsI_BhSZIIEVpNrlDVJZYqcHLQ",
+    authDomain: "perpbiz-9bebe.firebaseapp.com",
+    databaseURL: "https://perpbiz-9bebe.firebaseio.com",
+    projectId: "perpbiz-9bebe",
+    storageBucket: "perpbiz-9bebe.appspot.com",
+    messagingSenderId: "146535582528",
+    appId: "1:146535582528:web:21c5a85c291991dfe9c8a5",
+    measurementId: "G-EQM5W8WHXW"
+};
+
+export default firebaseConfig;
