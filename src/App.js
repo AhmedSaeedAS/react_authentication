2d376562ef3bb4df72861fb4840effb24d7b43b1
Ahmed Saeed <ahmed.saeed@tenpearls.com>
2020-01-22 16:52:51 +0500

    Persist token in local storage

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
 import axios from './axios'
 
-
 firebase.initializeApp(firebaseConfig);
+
 class App extends Component {
   state = {
     email: "",
@@ -21,81 +21,55 @@ class App extends Component {
     this.setState({ password: e.target.value })
   }
 
-  setUser = (user) => {
-    this.setState({ loggedInUser: user })
+  signIn = async () => {
+    let context = this;
+    const credentials = {
+      email: this.state.email,
+      password: this.state.password,
+      returnSecureToken: true
+    }
+    const login = await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4mYvAgHsI_BhSZIIEVpNrlDVJZYqcHLQ", credentials)
+    localStorage.setItem("token", login.data.idToken)
+    console.log(login);
+    context.setUser(login);
   }
-
   signOut = () => {
     let context = this;
-
     firebase.auth().signOut().then(function () {
       // Sign-out successful.
       console.log("Sign-out successful.")
+      localStorage.removeItem("token")
       context.setUser("")
       console.log()
     }).catch(function (error) {
       // An error happened.
     });
   }
-  count = 0;
 
-  signIn = async () => {
-    let context = this
-    const credentials = {
-      email: this.state.email,
-      password: this.state.password,
-      returnSecureToken: true
-    }
-    axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4mYvAgHsI_BhSZIIEVpNrlDVJZYqcHLQ", credentials)
-      .then(res => {
-        return (context.setUser(res),
-          console.log(res))
-      })
-      .catch(err =>
-        console.log(err))
-    // firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-    //   .then(
-    //     firebase.auth().onAuthStateChanged(function (user) {
-    //       debugger
-    //       context.count = context.count + 1
-    //       if (user) {
-    //         context.count = 0
-    //         context.setUser(user)
-    //         alert("sign In")
-    //       }
-    //       if (context.count > 0 && context.count < 3) {
-    //         context.signIn()
-    //       }
-    //     }))
-    //   .catch(function (error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     console.log(errorCode)
-    //     var errorMessage = error.message;
-    //     console.log(errorMessage)
-    //     return
-    //   });
+  setUser = (user) => {
+    this.setState({ loggedInUser: user })
   }
 
-
-  loadImage = async () => {
-    axios.get("/ImagesText.json?auth=" + this.state.loggedInUser.data.idToken).then(res =>
+  loadImageText = async () => {
+    axios.get("/ImagesText.json?auth=" + localStorage.getItem("token")).then(res =>
       (
-        // localStorage.setItem('imagesText', JSON.stringify(res.data)),
-        // this.setState(res.data)
         console.log(JSON.stringify(res.data))
       )
-    ).catch(err => (console.log(err)))
+    ).catch((err) => {
+      if (err.response.status === 401) {
+        this.signOut()
+      }
+    })
   }
 
   render() {
 
     return (
-      this.state.loggedInUser === "" ? <div className="App" >
+      !localStorage.getItem("token") ? <div className="App" >
         <input placeholder="email" type="email" onChange={(e) => this.setEmailHandler(e)} />
         <input placeholder="password" type="password" onChange={(e) => { this.setPwdHandler(e) }} />
         <button onClick={this.signIn}>Submit</button>
-      </div> : <div> <button onClick={this.loadImage}>Load Text</button> <button onClick={this.signOut}>Log Out</button></div>
+      </div> : <div> <button onClick={this.loadImageText}>Load Text</button> <button onClick={this.signOut}>Log Out</button></div>
     );
   }
 }
